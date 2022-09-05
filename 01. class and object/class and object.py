f199a4c341658284f52d3a9f3545b2a06f5e8afe
Mohamed Hosny <hosnysys@hotmail.com>
2022-09-05 03:49:16 +0200

    Add files via upload

---
+++ 01. class and object/class and object.py
@@ -0,0 +1,20 @@
+numbers = [4,3,2,5,1]
+print(numbers)
+numbers.sort()
+print(numbers)
+numbers.append(0)
+print(numbers)
+
+print(type(numbers))
+
+x = 1
+print(type(x))
+
+customer.add()
+customer.remove()
+customer.get_name()
+customer.get_order_total()
+
+point.draw()
+point.move()
+point.get_distance(1,2)
